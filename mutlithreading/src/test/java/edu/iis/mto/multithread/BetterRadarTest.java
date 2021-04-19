98449f5d99aacaebd4128b5973bf3532d405850b
Jan Góral <jan.goral112@gmail.com>
2021-04-19 19:26:09 +0200

    Added shouldExecuteBatteryLunchPatriotWithGivenEnemyMissileFiveTimes test and changed test class from RadarTest to BetterRadarTest

---
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -0,0 +1,37 @@
+package edu.iis.mto.multithread;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+
+@ExtendWith(MockitoExtension.class)
+class BetterRadarTest {
+
+    @Mock
+    private PatriotBattery batteryMock;
+
+    @RepeatedTest(100)
+    void shouldExecuteBatteryLunchPatriotWithGivenEnemyMissileFiveTimes() {
+        // given
+        int sampleRocketCount = 5;
+        Executor executor = Runnable::run;
+        BetterRadar betterRadar = new BetterRadar(batteryMock, sampleRocketCount, executor);
+        Scud enemyMissile = new Scud();
+        
+        doNothing().when(batteryMock).launchPatriot(enemyMissile);
+        // when
+        betterRadar.notice(enemyMissile);
+        
+        // then
+        verify(batteryMock, times(sampleRocketCount)).launchPatriot(enemyMissile);
+    }
+
+}
